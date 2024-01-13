d23884472c23e4f5ca3c1910f915c7f8814f63c3
Diego Ordonez <d.ordonez.morales@avanade.com>
2024-01-13 19:20:17 +0100

    TF dummy deployment

--- exercises/Lab_01/env.sh
+++ exercises/Lab_01/env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export TF_VAR_AZ_APPID="89ad41a3-b9e4-4ca7-81fa-157d4396ff0c"
-export TF_VAR_AZ_TENANTID="TENANT"
-export TF_VAR_AZ_SECRET="SECRET_VERY_HARD"
-export TF_VAR_AZ_SUBSID=""
+export TF_VAR_AZ_TENANTID="a277a833-af78-4a1e-abeb-32d974b6cf06"
+export TF_VAR_AZ_SECRET="u_l8Q~MQQoZLn3NbsD5dLclOdY9K57jzTw5pxaPI"
+export TF_VAR_AZ_SUBSID="dfc1c669-4460-40a1-8d6a-3469f2b10f23"
