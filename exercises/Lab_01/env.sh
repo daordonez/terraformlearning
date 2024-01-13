93cb228f182d2791a4363af431ce4df48df9722c
Diego Ordonez <d.ordonez.morales@avanade.com>
2024-01-13 10:37:13 +0100

    Az login implementation

---
+++ exercises/Lab_01/env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export AZ_APPID="89ad41a3-b9e4-4ca7-81fa-157d4396ff0c"
+export AZ_TENANTID="a277a833-af78-4a1e-abeb-32d974b6cf06"
+export AZ_SECRET="u_l8Q~MQQoZLn3NbsD5dLclOdY9K57jzTw5pxaPI"
--- exercises/Lab_01/run.sh
+++ exercises/Lab_01/run.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+echo "Loading ENV vars"
+. ./env.sh
+
+echo "Creating Azure connection"
+az login --service-principal -u $AZ_APPID -p $AZ_SECRET --tenant $AZ_TENANTID
