f1712fec33418bded58fc1160b3f364af092a2c2
Diego Ordonez <d.ordonez.morales@avanade.com>
2024-01-16 10:09:33 +0100

    vnet and subnet implementation + tagging

--- exercises/Lab_01/main.tf
+++ exercises/Lab_01/main.tf
@@ -15,16 +15,35 @@ terraform {
 provider "azurerm" {
   features {}
 
-  client_id       = var.AZ_APPID
-  client_secret   = var.AZ_SECRET
-  tenant_id       = var.AZ_TENANTID
-  subscription_id = var.AZ_SUBSID
+  client_id       = local.az.app
+  client_secret   = local.az.secret
+  tenant_id       = local.az.tenant
+  subscription_id = local.az.subscription
 }
 
 
 #1 resource creation
 # Resource group
 resource "azurerm_resource_group" "rg-01" {
-  name     = "TST-WESTEUR-TFLAB1"
+  name     = "TST-WEUR-TFLAB1"
   location = "westeurope"
+
+  tags = local.common_tags
+}
+
+#2. Create vnet & subnet
+resource "azurerm_virtual_network" "lab01-vnet" {
+  name = "TST-WEUR-VNET01"
+  address_space = [ "10.0.0.0/26" ]
+  location = "westeurope"
+  resource_group_name = azurerm_resource_group.rg-01.name
+
+  tags = local.common_tags
+}
+resource "azurerm_subnet" "lab01-subnet" {
+  name = "TST-WEUR-SUBNET01"
+  address_prefixes = ["10.0.0.0/27"]
+  virtual_network_name = azurerm_virtual_network.lab01-vnet.name
+  resource_group_name = azurerm_resource_group.rg-01.name
+
 }
--- exercises/Lab_01/variables.tf
+++ exercises/Lab_01/variables.tf
@@ -1,4 +1,31 @@
-variable "AZ_APPID" {}
+variable "AZ_APPID" {
+    sensitive = true
+}
 variable "AZ_TENANTID" {}
-variable "AZ_SECRET" {}
+variable "AZ_SECRET" {
+    sensitive = true
+}
 variable "AZ_SUBSID" {}
+
+variable "company" {
+  type = string
+  description = "Company name for tagging purpose"
+  default = "DordoLabs"
+}
+variable "project_name" {
+  type = string
+  description = "Project name for tagging purpose"
+  default = "Lab-01"
+}
+variable "billing_code" {
+  type = string
+  description = "Billing code for tagging purpose"
+  default = "201219DN"
+}
+
+variable "environment" {
+    type = string
+    description = "Defines deployment environment"
+    default = "TST"
+  
+}
