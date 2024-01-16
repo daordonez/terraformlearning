42eb1826997426a59f0c29968d18e59e058472f9
Diego Ordonez <d.ordonez.morales@avanade.com>
2024-01-16 10:09:33 +0100

    vnet and subnet implementation + tagging

---
+++ exercises/Lab_01/locals.tf
@@ -0,0 +1,15 @@
+locals {
+  az = {
+    app          = var.AZ_APPID
+    tenant       = var.AZ_TENANTID
+    secret       = var.AZ_SECRET
+    subscription = var.AZ_SUBSID
+  }
+
+  common_tags = {
+    Environment = var.environment
+    Company     = var.company
+    Project     = "${var.company}-${var.project_name}"
+    BillingCode = var.billing_code
+  }
+}
