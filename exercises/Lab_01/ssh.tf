5ef9f78b6603986ad82f7f0abd53398ff1bcfb2b
Diego Ordonez <ordonez.diego@outlook.com>
2024-01-18 13:47:02 +0100

    new nsg rule added for http-nginx service

--- exercises/Lab_01/ssh.tf
+++ exercises/Lab_01/ssh.tf
@@ -24,6 +24,6 @@ resource "azapi_resource_action" "ssh_public_key_gen" {
 }
 
 #Output key values
-output "key_data" {
-  value = jsondecode(azapi_resource_action.ssh_public_key_gen.output).publicKey
-}
+#output "key_data" {
+#  value = jsondecode(azapi_resource_action.ssh_public_key_gen.output).publicKey
+#}
