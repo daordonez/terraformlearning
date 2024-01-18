b6a8a96c88c4b7991e87a16cbcba432f5bfa6eb1
Diego Ordonez <ordonez.diego@outlook.com>
2024-01-18 13:55:55 +0100

    cleanning connection az strings

--- exercises/Lab_01/env.sh
+++ exercises/Lab_01/env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export TF_VAR_AZ_APPID="89ad41a3-b9e4-4ca7-81fa-157d4396ff0c"
-export TF_VAR_AZ_TENANTID="a277a833-af78-4a1e-abeb-32d974b6cf06"
-export TF_VAR_AZ_SECRET="u_l8Q~MQQoZLn3NbsD5dLclOdY9K57jzTw5pxaPI"
-export TF_VAR_AZ_SUBSID="dfc1c669-4460-40a1-8d6a-3469f2b10f23"
+export TF_VAR_AZ_APPID="<add-id>"
+export TF_VAR_AZ_TENANTID="<your-tenant-id>"
+export TF_VAR_AZ_SECRET="<your-Secret_very_strong>"
+export TF_VAR_AZ_SUBSID="<your-sub-id>"
