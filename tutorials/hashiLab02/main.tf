2043b3acadf45d288192c910df07305926564837
Diego Ordonez - MBP <diegordonez4@gmail.com>
2024-08-28 14:27:01 +0200

    AWS S3 bucket creation and provider upgrade (forced) OK

--- tutorials/hashiLab02/main.tf
+++ tutorials/hashiLab02/main.tf
@@ -1,6 +1,16 @@
-# The main.tf file in Terraform is a required configuration file that defines the infrastructure resources that should be created, modified or deleted. This file is used to declare the resources that make up your infrastructure in a declarative way.
+#Usually 'main.tf' file is intended to define resources. In this module either providers and terraform configuration
+#have been splitted in two different files.
 
 resource "random_pet" "pet" {
-  length = var.pet_name_length
-  prefix = var.pet_name_prefix
+  keepers = {
+    ami_id = var.pet_name_prefix
+  }
+}
+
+resource "aws_s3_bucket" "sample" {
+  bucket = "s3-${random_pet.pet.id}"
+
+  tags = {
+    public_bucket = false
+  }
 }
